b418773664d37b54f5c1020dd157d222967b9689
ecejbrr <ecejbrr@gmail.com>
2017-10-25 10:06:45 +0200

    hundreds with 0 polignac numbers output

--- polignac.py
+++ polignac.py
@@ -49,11 +49,16 @@ counter=0
 glob_counter=0
 t=tuple()
 
+hundreds_zero=list()
+
 # Loop over all natural numbers untill integer given as argument
 for i in range(2,int(max)):
     if i % 100 == 0:
         t=hundred*100, counter
+        # not smart, but valid since there are no polignac numbers in 0th hundred
         p_numbers.append(t)
+        if counter == 0:
+            hundreds_zero.append(t)
         hundred = hundred + 1
         counter = 0
 
@@ -88,7 +93,10 @@ for i in range(2,int(max)):
 t=hundred*100, counter
 p_numbers.append(t)
 
+#print(p_numbers)
+
 print("TOTAL POLIGNAC numbers found: " + str(glob_counter))
+print("HUNDREDs with 0 POLIGNAC numbers: " + str(hundreds_zero))
 
 # Remove following lines if no graph is required
 
